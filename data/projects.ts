8bc0f692584f12169e9365ded19b023319cb2010
Thabo Tshabalala <47thabo@gmail.com>
2025-03-16 19:36:30 +0200

    feat: add images and tech stack arrays and data

--- data/projects.ts
+++ data/projects.ts
@@ -2,11 +2,12 @@ export interface ProjectData {
   id: number;
   title: string;
   category: string;
-  image: string;
+  images: string[];
   description: string;
   github?: string;
   figma?: string;
   website?: string;
+  techStack?: string[]
 }
 
 export const projects: ProjectData[] = [
@@ -14,45 +15,70 @@ export const projects: ProjectData[] = [
     id: 1,
     title: 'TM System',
     category: 'WEB-APP',
-    image: '/images/projects/tmsystem.png',
+    images: [
+      "/images/projects/tmsystem.png",
+      "/images/projects/tmsystem-dashboard.png",
+      "/images/projects/tmsystem-tenants.png",
+    ],
     description: 'Web app for landlords to efficiently manage their properties and tenants.',
     github: 'https://github.com/Tshabalala-Thabo/TenantManagementSystem',
     figma: 'https://www.figma.com/proto/B61Ppmehgkogvp84zrOHkx/Tenant-Management-System?node-id=1-2&starting-point-node-id=1%3A2&t=oWnFpvzlhwP0GhV1-1',
+    techStack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
   },
   {
     id: 2,
     title: 'Nationality Predictor',
     category: 'WEB-APP',
-    image: '/images/projects/nationality-predictor.png',
+    images: [
+      '/images/projects/nationality-predictor.png',
+      "/images/projects/tmsystem-dashboard.png",
+      "/images/projects/tmsystem-tenants.png",
+    ],
     description: 'A sleek web app that predicts nationalities based on names, featuring real-time probability scores and animated visualizations.',
     github: 'https://github.com/Tshabalala-Thabo/nationality-predictor',
     website: 'https://nationality-predictor.vercel.app/',
+    techStack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
   },
   {
     id: 3,
     title: 'Danny Niches',
     category: 'WEBSITE',
-    image: '/images/projects/dn.png',
+    images: [
+      '/images/projects/nationality-predictor.png',
+      "/images/projects/tmsystem-dashboard.png",
+      "/images/projects/tmsystem-tenants.png",
+    ],
     description: 'Responsive corporate website with modern design, showcasing company services and portfolio.',
     github: 'https://github.com/Tshabalala-Thabo/danny-niches-react',
     website: 'https://dannyniches.co.za',
+    techStack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
   },
   {
     id: 5,
     title: 'Lagiva Vine',
     category: 'WEBSITE',
-    image: '/images/projects/lv.png',
+    images: [
+      '/images/projects/lv.png',
+      "/images/projects/tmsystem-dashboard.png",
+      "/images/projects/tmsystem-tenants.png",
+    ],
     description: 'Crafted an elegant wine website for enthusiasts to explore wines.',
     website: 'https://mrn-b453f.vercel.app',
+    techStack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
   },
   {
     id: 6,
     title: 'Boii Teddy',
     category: 'WEB-APP',
-    image: '/images/projects/boiiteddy.png',
+    images: [
+      '/images/projects/boiiteddy.png',
+      "/images/projects/tmsystem-dashboard.png",
+      "/images/projects/tmsystem-tenants.png",
+    ],
     description: 'A React-based music player web app for artist Boii Teddy, featuring track playback, social links, and smooth user experience.',
     github: 'https://github.com/Tshabalala-Thabo/boy-teddy-react.git',
     website: 'https://boiiteddy.vercel.app/',
+    techStack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
   },
 ];
 
